d16174c29c896ec2e12570ccd8967ad950ae80ed
OCHIENGHerman <ochiengherman32@gmail.com>
2024-03-01 15:40:59 +0300

    solve the error

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("postgres://herman_user:sPi1Y2ChuhntYH0hUBnrmF6ohAEvi1i4@dpg-cndq3mf79t8c738f0ehg-a.oregon-postgres.render.com:5432/herman");
+const sequelize = new Sequelize("postgres://herman_user:sPi1Y2ChuhntYH0hUBnrmF6ohAEvi1i4@dpg-cndq3mf79t8c738f0ehg-a.oregon-postgres.render.com:5432/herman", {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
 
 async function testConnection() {
     try {
